beddcef30d1bcd6247242bcfea12de2ed2c5033d
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-09-13 02:41:55 +0300

    added creat event method

--- contracts/event.sol
+++ contracts/event.sol
@@ -2,21 +2,44 @@
 pragma solidity ^0.8.24;
 
 contract NftEventGator{
-
+    address owner;
     struct Event{
         bool isActive;
         address NftAddress; // a specific nft for a specific event
-        uint256 id;
+        // uint256 id;
         uint256 startTime;
         uint256 endTime;
-        uint256 maxParticcipants;
+        uint256 maxParticipants;
         uint256 price;
         string location;
         string name;
         
     }
+    
+    constructor(){
+        owner = msg.sender;
+    }
 
-    function createEvent() external {}
+    // mapping(address => Event) public events;
+
+    Event[] public events;
+
+    function createEvent(string memory _name,uint256 _price, uint256 _startTime, uint256 _endTime, uint256 _maxParticipants, string memory _location, address _nftAddress ) external {
+        require(_startTime < _endTime, "");
+
+        Event memory newEvent = Event({
+            name: _name,
+            price: _price,
+            startTime: _startTime,
+            endTime: _endTime,
+            maxParticipants: _maxParticipants,
+            location: _location,
+            NftAddress: _nftAddress,
+            isActive: true
+        });
+
+        events.push(newEvent);
+    }
 
     //users registering for the event
     function claimingTickets() external{}
