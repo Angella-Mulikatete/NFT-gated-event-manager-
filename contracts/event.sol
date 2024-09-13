c904a01741aa1badcb22d590eac93fea02dc595f
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-09-13 21:27:44 +0300

    added resgister for event

--- contracts/event.sol
+++ contracts/event.sol
@@ -1,69 +1,96 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
+import {MyNft} from  './nft.sol';
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract NftEventGator{
     address owner;
+    IERC721  NFTAddress;
+    address participantAddress;
+    uint256 eventidCount;
+    string url;
+    uint256 tokenId;
+
+    constructor(address _nftAddress){
+        NFTAddress = IERC721(_nftAddress);
+    }
+
     struct Event{
         bool isActive;
-        address NftAddress; // a specific nft for a specific event
-        uint256 id;
+        address NFTAddress; 
         uint256 startTime;
         uint256 endTime;
         uint256 maxParticipants;
         uint256 price;
+        uint256 registeredCount;
         string location;
         string name;
         string organizer;
         
     }
 
-    mapping(uint => Event) public eventExists;
+
+    // uint256 nextEventId;
+
+    mapping(uint256 => Event) public events;
+    // mapping(address => uint256) public participants; //mapping address to id of participants
+    mapping(uint256 => mapping(address => bool)) registeredParticipants; //eventId => participantAddress => boolean
 
     modifier onlyOrganizer {
-        require(msg.sender ==owner, "not owner");
+        require(msg.sender == owner, "not owner");
         _;
     }
 
-    modifier eventExist(uint256 _id){
-        require((eventExists[_id]).id != 0, "event doesnot exist");
+    modifier canRegister(uint256 eventId){
+        require(events[eventId].registeredCount < events[eventId].maxParticipants, "Event is full"); //checking if the event is not full
+        require(!registeredParticipants[eventId][msg.sender], "Already Registered");
         _;
     }
 
-    // modifier isRegistered(uint256 _id){
-    //     require
-    // }
-
-  
-
-    constructor(){
-        owner = msg.sender;
+    modifier eventExists(uint256 eventId){
+        require(!events[eventId].isActive, "Event doesnot exist");
+        _;
     }
 
-    // mapping(address => Event) public events;
+    modifier hasNft(uint256 eventId){
+        require(IERC721(events[eventId].NFTAddress).balanceOf(msg.sender) > 0, "There is no Nft yet");
+        _;
+    }
 
-    Event[] public events;
-
-    function createEvent(string memory _name,uint256 _price, uint256 _startTime, uint256 _endTime, uint256 _maxParticipants, string memory _location, address _nftAddress ) external onlyOrganizer {
+    function createEvent(string memory _name, uint256 _price, uint256 _startTime, uint256 _endTime,  string memory _location, address _nftAddress, string memory _organizer ) external onlyOrganizer {
         // require(owner != 0, "zero address detected");
         require(_startTime < _endTime, "");
 
-        Event memory newEvent = Event({
+        eventidCount++; //the event Id is incremented so as we dont have the same ids, initiall the id is 0
+
+        events[eventidCount]= Event({
             name: _name,
             price: _price,
             startTime: _startTime,
             endTime: _endTime,
-            maxParticipants: _maxParticipants,
+            NFTAddress: _nftAddress,
+            maxParticipants: 0,
             location: _location,
-            NftAddress: _nftAddress,
-            isActive: true
+            isActive: true,
+            organizer: _organizer,
+            registeredCount: 0
         });
 
-        events.push(newEvent);
+
+        
     }
 
     //users registering for the event
-    function registerForEvent(uint256 eventId) external {
-       
+    function registerParticipants(uint256 eventId) external  canRegister(eventId) eventExists(eventId) hasNft(eventId){
+       events[eventId].registeredCount ++;
+       registeredParticipants[eventId][msg.sender] == true;
+    }
+
+    function mintNft(address _participantAddress) internal onlyOrganizer returns(uint256){
+      tokenId =   NFTAddress.mint(_participantAddress);
+      tokenId++;
+      return tokenId;
     }
 
     //when users attend the event, we need to verify their nft
--- contracts/nft.sol
+++ contracts/nft.sol
@@ -6,18 +6,20 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
-    constructor(address initialOwner)
-        ERC721("MyToken", "MTK")
-        Ownable(initialOwner)
-    {}
+contract MyNft is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    constructor() ERC721("MyNft", "MNT") Ownable(msg.sender){}
 
-    function safeMint(address to, uint256 tokenId, string memory uri)
-        public
-        onlyOwner
+    uint256 _tokenIds;
+
+    function mint(address to)public onlyOwner returns(uint256) 
     {
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        _tokenIds++;
+        uint256 newTokenId = _tokenIds;
+        _safeMint(to, newTokenId);
+        
+        // _setTokenURI( newTokenId, uri);
+
+        return newTokenId;
     }
 
     // The following functions are overrides required by Solidity.
